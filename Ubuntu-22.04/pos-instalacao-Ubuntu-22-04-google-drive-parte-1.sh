549fdfb1ecd19c964f6b37fc4ffbd0ad7638cb71
Luciano <luc.oliveira343@gmail.com>
2023-08-14 03:36:55 -0300

    bosta

--- Ubuntu-22.04/pos-instalacao-Ubuntu-22-04-google-drive-parte-1.sh
+++ Ubuntu-22.04/pos-instalacao-Ubuntu-22-04-google-drive-parte-1.sh
@@ -221,10 +221,18 @@ if [ -e docker-compose ];then
     echo "O arquivo  docker-compose  ja existe"
 else
   echo -e "\n\n\n\n Docker Compose"
+<<<<<<< HEAD
   wget --load-cookies /tmp/cookies.txt \
   "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate \
       'https://docs.google.com/uc?export=download&id=1q-vqLH0LfMasksSVytw7HfkTrOxJlCyA' -O- | \
   sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1q-vqLH0LfMasksSVytw7HfkTrOxJlCyA" -O docker-compose && rm -rf /tmp/cookies.txt \;
+=======
+  fileId=1q-vqLH0LfMasksSVytw7HfkTrOxJlCyA
+  fileName=docker-composeplayer
+  curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${fileId}" > /dev/null
+  code="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
+  curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=${fileId}" -o ${fileName}
+>>>>>>> parent of a61e8eb (consertando o nome do docker compose)
 fi
 
 
