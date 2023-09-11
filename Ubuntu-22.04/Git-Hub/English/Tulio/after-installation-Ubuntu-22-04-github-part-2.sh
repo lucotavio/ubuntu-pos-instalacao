73fb2dc775f38219de8ea134019acf554a6b8cdb
Luciano <luc.oliveira343@gmail.com>
2023-09-11 13:26:04 -0300

    add files to installation in notebook of Tulio and remove the coments @todo in creation of folder OBS_Studio

---
+++ Ubuntu-22.04/Git-Hub/English/Tulio/after-installation-Ubuntu-22-04-github-part-2.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+
+
+## Entrando na para pasta de programas
+cd /home/$USER/Downloads/Softwares/
+
+
+
+
+echo -e "\n\n\n *************************************  INSTALANDO  SPRING TOOLS  SUITE  COM  LOMBOK **************************************************"
+
+## Spring tool Suite
+sudo dpkg -i spring-tool-suite-4.19.1.RELEASE.deb
+sudo apt --fix-broken install -y
+
+## Instalando Lombok no Spring Tools Suite
+chmod +x lombok.jar
+sudo java -jar lombok.jar
+
+echo "***********************************************************************************************************************************************"
+
+
+
+
+echo -e "\n\n\n************************************************** INSTALANDO  ARQUIVOS  .DEB ********************************************************"
+
+## Tornando todos os arquivos .deb
+## em arquivos executavies
+chmod +x *.deb
+
+
+## Atom
+sudo dpkg -i atom-amd64.deb
+sudo apt --fix-broken install -y
+sudo sed -i 's\env ATOM_DISABLE_SHELLING_OUT_FOR_ENVIRONMENT=false /usr/bin/atom %F\env ATOM_DISABLE_SHELLING_OUT_FOR_ENVIRONMENT=false /usr/bin/atom %F --in-process-gpu\g' /usr/share/applications/atom.desktop
+
+
+## Balena Etcher
+sudo dpkg -i balena-etcher_1.18.11_amd64.deb
+sudo apt --fix-broken install -y
+
+
+## Chrome
+sudo dpkg -i chrome-stable_current_amd64.deb
+sudo apt --fix-broken install -y
+
+
+## Dbeaver
+sudo dpkg -i dbeaver-le_23.1.0_amd64.deb
+sudo apt --fix-broken install -y
+
+
+## Firefox
+sudo dpkg -i firefox-116.0.2.deb
+sudo apt --fix-broken install -y
+
+
+## Insomnia
+sudo dpkg -i Insomnia.Core-2023.4.0.deb
+sudo apt --fix-broken install -y
+
+
+## Intellij Community
+sudo dpkg -i intellij-community-2023.2.deb
+sudo apt --fix-broken install -y
+
+
+## Microsoft Edge
+sudo dpkg -i microsoft-edge-stable_116.0.1938.76-1_amd64.deb
+sudo apt --fix-broken install -y
+
+
+## MySql Workbench
+sudo dpkg -i mysql-workbench-community_8.0.34-1ubuntu22.04_amd64.deb
+sudo apt --fix-broken install -y
+
+
+## Only Office
+sudo dpkg -i onlyoffice-desktopeditors_amd64.deb
+sudo apt --fix-broken install -y
+sudo sed -i '/Terminal=false/a StartupWMClass=DesktopEditors' /usr/share/applications/onlyoffice-desktopeditors.desktop
+
+
+## Postman
+sudo dpkg -i postman-10.17.1.deb
+sudo apt --fix-broken install -y
+
+
+## Visual Studio Code
+sudo dpkg -i code_1.81.1-1691620686_amd64.deb
+sudo apt --fix-broken install -y
+
+
+############################################################## INSTALANDO  APACHE  NETBEANS #########################################################
+
+## Apache Netbeans
+sudo dpkg -i apache-netbeans_18-1_all.deb
+sudo apt --fix-broken install -y
+
+
+## Baixando repositorio Classes do Git Hub
+if [ -d /home/$USER/.netbeans/18/config/Templates/Classes/ ];then
+    echo "O diretorio  /home/$USER/.netbeans/18/config/Templates/Classes/  ja existe"
+else
+
+  ## Criando a pasta onde vao ficar os templates modificados
+  mkdir -p /home/$USER/.netbeans/18/config/Templates/
+
+  echo -e "\n\n\n\n Clonando repositorio Classes do Git Hub"
+  git clone https://github.com/lucotavio/Classes.git
+  cp -r Classes/ /home/$USER/.netbeans/18/config/Templates/
+fi
+
+########################################################## FIM DA  INSTALACAO  DO  APACHE  NETBEANS##################################################
+
+echo "***********************************************************************************************************************************************"
+
+
+
+
+## Atualizando a maneira de atualizar as keys dos repositorio sao salva
+sudo cp /etc/apt/trusted.gpg /etc/apt/trusted.gpg.d
+
+
+## Deletando dependencias que nao estao sendo utilizadas
+sudo apt autoremove -y
+
+echo -e "\n\n\n****************************************** REINICIAR  O  COMPUTADOR********************************************************************"
